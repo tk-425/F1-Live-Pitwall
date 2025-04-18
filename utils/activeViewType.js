a6d3a641187eb29c18bcf42e48244c4c74ddf4cf
trry <54043933+tk-425@users.noreply.github.com>
2025-04-18 14:59:08 -0400

    first commit

---
+++ utils/activeViewType.js
@@ -0,0 +1,5 @@
+export const ActiveViewType = {
+  INTERVALS: '3-Sec Intervals',
+  POSITIONS: 'Positions',
+  TEAM_RADIO: 'Team Radio',
+};
---
+++ utils/sendDataType.js
@@ -0,0 +1,10 @@
+export const SendDataType = {
+  INTERVALS: 'intervals',
+  ALL_INTERVALS: 'all_intervals',
+  POSITIONS: 'positions',
+  SESSION: 'session',
+  STINTS: 'stints',
+  TEAM_RADIO: 'team_radio',
+  MEETING: 'meeting',
+  SCHEDULE: 'schedule',
+};
