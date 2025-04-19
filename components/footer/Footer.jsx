071f9963d5eac37dd40c11a9ab382cdb175a9adb
trry <54043933+tk-425@users.noreply.github.com>
2025-04-18 22:22:29 -0400

    update footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <div className='my-4 px-4'>
+    <div className='px-4 mb-4'>
       <div className='text-center mt-2 text-xs/4 text-gray-500'>
+        <p className='mb-3'>
+          <Link className='text-blue-600 mr-1' href='https://github.com/tk-425/F1-Live-Pitwall-WebSocket'>
+            Github
+          </Link>
+          Version: 0.0.4
+        </p>
         <p>
           This project/website is unofficial and is not associated in any way
           with the Formula 1 companies.
--- components/teamRadio/TeamRadio.jsx
+++ components/teamRadio/TeamRadio.jsx
@@ -1,63 +1,3 @@
-// import Image from 'next/image';
-// import { useWebSocketContext } from '@/context/WebSocketContext';
-// import DriverRadioPlayer from './DriverRadioPlayer';
-// import { combineTeamRadiosByConstructor } from '@/utils/flattenRadioTracks';
-// import { constructorIcons } from '@/info/utils/constructorIcons';
-// import { teamIconFit } from '@/style/style';
-// import Unavailable from '../utils/Unavailable';
-// import { ActiveViewType } from '@/utils/activeViewType';
-// import { useEffect, useState } from 'react';
-// import { isValidArray } from '@/utils/dataUtils';
-// import { Loading } from '../utils/Loading';
-
-// export default function TeamRadioPlayer() {
-//   const { teamRadio } = useWebSocketContext();
-//   const teamRadioByConstructor = combineTeamRadiosByConstructor(teamRadio);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     setLoading(false);
-//   }, [teamRadio]);
-
-//   if (loading) {
-//     return <Loading />;
-//   }
-
-//   if (!isValidArray(teamRadio)) {
-//     return <Unavailable message={ActiveViewType.TEAM_RADIO} />;
-//   }
-
-//   return (
-//     <div className='mt-6 mb-4'>
-//       {Object.keys(teamRadioByConstructor).map((constructor) => (
-//         <div key={constructor}>
-//           <div className='flex text-lg items-center font-bold'>
-//             <Image
-//               src={constructorIcons[constructor]}
-//               width={26}
-//               height={26}
-//               className={teamIconFit}
-//               alt={`${constructor} || 'constructor-logo`}
-//             />
-//             <span>{constructor}</span>
-//           </div>
-//           <div className='mb-3'>
-//             {Object.values(teamRadioByConstructor[constructor]).map(
-//               (driver) => (
-//                 <DriverRadioPlayer
-//                   key={driver.driver_number}
-//                   driverNumber={driver.driver_number}
-//                   radios={driver.radios}
-//                 />
-//               )
-//             )}
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
 import Image from 'next/image';
 import { useWebSocketContext } from '@/context/WebSocketContext';
 import DriverRadioPlayer from './DriverRadioPlayer';
@@ -86,39 +26,6 @@ export default function TeamRadioPlayer() {
   if (!isValidArray(teamRadio)) {
     return <Unavailable message={ActiveViewType.TEAM_RADIO} />;
   }
-
-  // return (
-  //   <div className='mt-6 mb-4'>
-  //     {Object.keys(teamRadioByConstructor).map((constructor) => (
-  //       <div key={constructor}>
-  //         {/* Title */}
-  //         <div className='flex items-center font-bold text-base md:text-lg mb-2'>
-  //           <Image
-  //             src={constructorIcons[constructor]}
-  //             width={22}
-  //             height={22}
-  //             className={`${teamIconFit} w-6 h-6 md:w-8 md:h-8 mr-2`}
-  //             alt={`${constructor} || 'constructor-logo'`}
-  //           />
-  //           <span className='truncate'>{constructor}</span>
-  //         </div>
-
-  //         {/* Radios */}
-  //         <div className='mb-6'>
-  //           {Object.values(teamRadioByConstructor[constructor]).map(
-  //             (driver) => (
-  //               <DriverRadioPlayer
-  //                 key={driver.driver_number}
-  //                 driverNumber={driver.driver_number}
-  //                 radios={driver.radios}
-  //               />
-  //             )
-  //           )}
-  //         </div>
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
 
   return (
     <div className='mt-6 mb-4'>
